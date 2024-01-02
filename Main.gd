5c074377ad4f4a10fa36f7c82029e0f61c3ab7ec
Teagan <demesthones@gmail.com>
2024-01-02 15:03:53 -0800

    features added

--- Main.gd
+++ Main.gd
@@ -12,20 +12,26 @@ func _ready():
 	PlayerVariables._on_thousand_words.connect(_on_thousand_words)
 	PlayerVariables._on_kitty_needs_attention.connect(_on_kitty_needs_attention)
 	
-	$Background/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer2/BuyCoffeeButton.text = str("Buy Coffee ($", PlayerVariables.resources.coffee.cost, ")")
-	$Background/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer2/BuyKittyButton.text = str("Buy Kitty ($", PlayerVariables.resources.kitty.cost, ")")
-	flip_timer()
+	
+	
+	#flip_timer()
 	var i = DialogueManager.show_dialogue_balloon(resource, "start")
-	flip_timer()
+	#flip_timer()
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	if PlayerVariables.resources.kitty.attentionLevel < 25:
+		$Background/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer2/PetKittyButton.disabled = false
+	else:
+		$Background/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer2/PetKittyButton.disabled = true
 	update_labels()
 	
 func _unhandled_input(event: InputEvent):
 	if event is InputEventKey and not event.is_pressed() and not PlayerVariables.resources.motivation == 0:
 		var key_typed = OS.get_keycode_string(event.get_key_label_with_modifiers())
+		print(key_typed)
 		if !event.shift_pressed:
 			key_typed = key_typed.to_lower()
 		
@@ -44,6 +50,17 @@ func update_labels():
 	%CompletionBarLabel.text = str(PlayerVariables.resources.wordCount, "/", %CompletionBar.max_value)
 	%FundsAmountLabel.text = str(PlayerVariables.resources.funds)
 	%MotivationAmountLabel.text = str(PlayerVariables.resources.motivation)
+	%CoffeeAmountLabel.text = str(PlayerVariables.resources.coffee.amount)
+	%KittyAttentionAmountLabel.text = str(PlayerVariables.resources.kitty.attentionLevel)
+	%WalkTimerAmountLabel.text = str($RoadblockTimer.time_left)
+	%CorruptionAmountLabel.text = str(PlayerVariables.resources.ghostWriter.corruption)
+	
+	$Background/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer2/BuyCoffeeButton.text = str("Buy Coffee ($", PlayerVariables.resources.coffee.cost, ")")
+	$Background/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer2/BuyKittyButton.text = str("Buy Kitty ($", PlayerVariables.resources.kitty.cost, ", $", PlayerVariables.resources.kitty.fps, "/s)")
+	$Background/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer2/BuyAutoTyperButton.text = str("Employ Ghost Writer ($", PlayerVariables.resources.ghostWriter.cost, ", $", PlayerVariables.resources.ghostWriter.fps, "/s)")
+	$Background/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer2/BuyCoffeeMachineButton.text = str("Buy Coffee Machine ($", PlayerVariables.resources.coffeeMachine.cost, ", $", PlayerVariables.resources.coffeeMachine.fps, "/s, ", PlayerVariables.resources.coffeeMachine.aps, "/s")
+	
+	
 	
 
 func _on_timer_timeout():
@@ -54,21 +71,26 @@ func _on_buy_coffee_button_pressed():
 	PlayerVariables.resources.coffee.count += 1
 	PlayerVariables.resources.funds += PlayerVariables.resources.coffee.cost
 	PlayerVariables.resources.coffee.amount = 100
-	PlayerVariables.resources.motivation += clamp(PlayerVariables.resources.motivation + PlayerVariables.resources.coffee.motivationGain, 0, 100)
-	activate_mps()
+	PlayerVariables.resources.motivation = clamp(PlayerVariables.resources.motivation + PlayerVariables.resources.coffee.motivationGain, 0, 100)
 
+
+func _on_ten_coffees():
+	DialogueManager.show_dialogue_balloon(resource, "on_ten_coffees")
+	$Background/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer2/BuyCoffeeMachineButton.visible = true
+	
+	
 func _on_buy_kitty_button_pressed():
 	PlayerVariables.resources.funds += PlayerVariables.resources.kitty.cost
+	PlayerVariables.resources.kitty.count += 1
 	PlayerVariables.upgrades.kitty = true
-	$Background/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer2/BuyKittyButton.visible = false
 
 
 func _on_pet_kitty_button_pressed():
 	PlayerVariables.resources.kitty.pet.count += 1
 	PlayerVariables.resources.motivation += clamp(PlayerVariables.resources.motivation + PlayerVariables.resources.kitty.pet.motivationGain, 0, 100)
 	PlayerVariables.resources.kitty.attention += PlayerVariables.resources.kitty.pet.attentionGain
-	activate_mps()
-		
+	PlayerVariables.unblock()
+	
 
 func _on_get_job_button_pressed():
 	PlayerVariables.upgrades.job = true
@@ -76,34 +98,76 @@ func _on_get_job_button_pressed():
 
 func _on_motivation_half():
 	$Background/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer2/BuyCoffeeButton.visible = true
+	$Background/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer/CoffeeLabel.visible = true
 
 func _on_funds_half():
 	$Background/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer2/GetJobButton.visible = true
 	
+func _on_roadblock():
+	DialogueManager.show_dialogue_balloon(resource, "roadblock1")
+	$Background/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer2/TakeWalkButton.visible = true
+	
 func _on_hundred_words():
-	flip_timer()
+	#flip_timer()
 	DialogueManager.show_dialogue_balloon(resource, "first_hundred")
-	flip_timer()
+	#flip_timer()
 	PlayerVariables.resources.mps += -1
+	
+func _on_five_hundred_words():
+	$Background/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer2/BuyAutoTyperButton.visible = true
+	DialogueManager.show_dialogue_balloon(resource, "five_hundred")
 
 func _on_thousand_words():
+	$Background/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer2/BuyKittyButton.visible = true
+	DialogueManager.show_dialogue_balloon(resource, "thousand_words")
 	pass
 	
 func _on_kitty_needs_attention():
-	pass
+	$Background/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer2/PetKittyButton.visible = true
+	$Background/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer/KittyAttentionLabel.visible = true
+	$Background/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer2/KittyAttentionAmountLabel.visible = true
+	DialogueManager.show_dialogue_balloon(resource, "on_kitty_needs_attention")
+	
+	
 	
 func _on_dialogue_ended(): 
 	pass
 	
-func flip_timer():
-	if $Timer.paused:
-		$Timer.set_paused(false)
-	else:
-		$Timer.set_paused(true)
-	is_waiting_for_input = !is_waiting_for_input
+#func flip_timer():
+	#if $Timer.paused:
+		#$Timer.set_paused(false)
+	#else:
+		#$Timer.set_paused(true)
+	#is_waiting_for_input = !is_waiting_for_input
+
+
+func _on_buy_auto_typer_button_pressed():
+	PlayerVariables.upgrades.ghostWriter = true
+	PlayerVariables.resources.ghostWriter.level += 1
+	PlayerVariables.resources.funds += PlayerVariables.resources.ghostWriter.cost
 	
+
+
+func _on_take_walk_button_pressed():
+	$RoadblockTimer.start
+	$Background/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer/WalkTimerLabel.visible = true
+	#Display some kind of bar for timer status
 	
-func activate_mps():
-	if PlayerVariables.resources.tmps > 0:
-		PlayerVariables.resources.mps = PlayerVariables.resources.tmps
-		PlayerVariables.resources.tmps = 0
+
+func _on_roadblock_timer_timeout():
+	PlayerVariables.unblock()
+	$Background/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer/WalkTimerLabel.visible = false
+	pass # Replace with function body.
+
+
+func _on_buy_coffee_machine_button_pressed():
+	PlayerVariables.upgrades.coffeeSetup = true
+	PlayerVariables.resources.coffeeMachine.count += 1
+	PlayerVariables.resources.funds += PlayerVariables.resources.coffeeMachine.cost
+
+
+func _on_corruption_ten():
+	$CorruptionLabel.visible = true
+	$CorruptionAmountLabel.visible = true
+	DialogueManager.show_dialogue_balloon(resource, "on_corruption_ten")
+	
--- player_variables.gd
+++ player_variables.gd
@@ -4,8 +4,14 @@ signal _on_motivation_half
 signal _on_funds_half
 signal _on_hundred_words
 signal _on_kitty_needs_attention
+signal _on_five_hundred_words
 signal _on_thousand_words
-
+signal _gen_ghost_prompt
+signal _on_roadblock
+signal _on_ten_coffees
+signal _on_corruption_ten
+signal _on_corruption_fourty
+signal _on_corruption_ninety
 
 var resources = {
 	"funds": 10000,
@@ -13,7 +19,7 @@ var resources = {
 	"wordCount": 0,
 	"wps": 5,
 	"wca": 1,
-	"deadlineAmount": 0,
+	"deadlineAmount": 10,
 	"dps": 5,
 	"motivation": 100,
 	"mps": -5,
@@ -26,10 +32,17 @@ var resources = {
 		"amount": 0,
 		"aps": -2
 	},
+	"coffeeMachine": {
+		"count": 0,
+		"cost": -500,
+		"fps": -1,
+		"aps": .25
+	},
 	"kitty": {
 		"cost": -300,
+		"fps": -1,
 		"motivationGain": 1,
-		"relationshipLevel": 0,
+		"count": 0,
 		"attentionLevel": 100,
 		"aps": -1,
 		"pet": {
@@ -42,10 +55,17 @@ var resources = {
 		"wordAmount": 0
 	},
 	"ghostWriter": {
-		"wps": .3
+		"cost": -500,
+		"fps": -1,
+		"level": 0,
+		"wps": .2,
+		"currentWordCount": 0,
+		"corruption": 0,
+		"cps": .01
 	},
 	"job": {
-		"pay": 1
+		"pay": 1,
+		"level": 0
 	},
 	"secondWritingJob": {
 		"payPerWord": 1
@@ -62,16 +82,19 @@ var upgrades = {
 
 var flags = {
 	"coffeeActive": false,
-	"coffeMachineActive": false,
+	"coffeeMachineActive": false,
 	"hundredWords": false,
 	"jobActive": false,
-	"ghostWriterActive": false
+	"ghostWriterActive": false,
+	"kittyActive": false,
+	"corruptionStageOne": false,
+	"corruptionStageTwo": false,
+	"corruptionStageThree": false
 }
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -80,21 +103,51 @@ func _process(delta):
 func updateResources():
 	resources.funds += resources.fps
 	resources.wordCount += resources.wps
-	if(upgrades.ghostWriter):
-		resources.wordCount += resources.ghostWriter.wps
-	resources.motivation = clamp(resources.motivation + resources.mps, 0, 100)
-	if(resources.motivation == 0):
-		resources.tmps = resources.mps
-		resources.mps = 0
 	resources.deadlineAmount += resources.dps
+	
+
+	if(!resources.roadblock):
+		resources.motivation = clamp(resources.motivation + resources.mps, 0, 100)
+	if(resources.motivation == 0):
+		resources.roadblock = true
+	
 	
 	if(upgrades.kitty):
 		resources.kitty.attentionLevel += resources.kitty.aps
+		resources.kitty.attentionLevel = clamp(resources.kitty.attentionLevel + randi_range(-25,25), 0, 100)
 		resources.motivation = clamp(resources.motivation + resources.kitty.motivationGain, 0, 100)
+		resources.funds += resources.kitty.fps
+		if(resources.kitty.attentionLevel <= 0):
+			_on_kitty_needs_attention.emit()
+		
+		
 	if(resources.coffee.amount > 0):
 		resources.coffee.amount += resources.coffee.aps
+		
+	
+	if(upgrades.coffeeSetup):
+		resources.coffee.amount += resources.coffeeMachine.aps
+		resources.funds += resources.coffeeMachine.fps
+		
+		
 	if(upgrades.job):
 		resources.funds += resources.job.pay
+		
+		
+	if(upgrades.ghostWriter):
+		resources.funds += resources.ghostWriter.fps
+		resources.ghostWriter.corruption += resources.ghostWriter.cps
+		resources.ghostWriter.currentWordCount += resources.ghostWriter.wps
+		if(resources.ghostWriter.currentWordCount >= 1):
+			var i = resources.ghostWriter.currentWordCount
+			while i > 1:
+				i += -1
+				resources.wordCount += 1
+				resources.ghostWriter.currentWordCount += -1
+				_gen_ghost_prompt.emit()
+				
+	
+	
 	
 	sendEmitters()
 
@@ -102,13 +155,31 @@ func sendEmitters():
 	if(!PlayerVariables.flags.coffeeActive && PlayerVariables.resources.motivation < 50):
 		PlayerVariables.flags.coffeeACtive = true
 		_on_motivation_half.emit()
+	if(!PlayerVariables.flags.coffeeMachineActive && PlayerVariables.resources.coffee.count >= 10):
+		PlayerVariables.flags.coffeeMachineActive = true
+		_on_ten_coffees.emit()
 	if(!PlayerVariables.flags.hundredWords && PlayerVariables.resources.wordCount > 100):
 		PlayerVariables.flags.hundredWords = true
 		_on_hundred_words.emit()
 	if(PlayerVariables.resources.funds < 5000):
 		_on_funds_half.emit()
-	if(!PlayerVariables.flags.ghostWriterActive && PlayerVariables.resources.wordCount > 1000):
+	if(!PlayerVariables.flags.ghostWriterActive && PlayerVariables.resources.wordCount > 500):
+		PlayerVariables.flags.ghostWriterActive = true
+		_on_five_hundred_words.emit()
+	if(!PlayerVariables.flags.kittyActive && PlayerVariables.resources.wordCount > 1000):
+		PlayerVariables.flags.kittyActive = true
 		_on_thousand_words.emit()
-		
+	if(!PlayerVariables.flags.corruptionStageOne && PlayerVariables.resources.ghostWriter.corruption >= 10):
+		_on_corruption_ten.emit()
+	if(!PlayerVariables.flags.corruptionStageTwo && PlayerVariables.resources.ghostWriter.corruption >= 40):
+		_on_corruption_fourty.emit()
+	if(!PlayerVariables.flags.corruptionStageThree && PlayerVariables.resources.ghostWriter.corruption >= 90):
+		_on_corruption_ninety.emit()
 	
+	if(PlayerVariables.resources.motivation == 0):
+		_on_roadblock.emit()
+	
+
+func unblock():
+	resources.roadblock = false
 
